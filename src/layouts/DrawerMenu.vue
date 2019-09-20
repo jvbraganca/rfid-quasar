44e71885b772a9a14d128aea45f074db60f28e6b
jvbraganca@gmail.com <Cruhfiel13>
2019-09-20 06:42:11 -0300

    Commit inicial

---
+++ src/layouts/DrawerMenu.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-layout view="hHh lpR lff">
+    <q-header elevated class="bg-positive text-white">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+        <q-toolbar-title>
+          IFMG - RFID
+        </q-toolbar-title>
+        <div>
+          <q-btn flat color="white" label="Login" />
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-scroll-area class="fit">
+
+        <q-list v-for="(menuItem, i) in menuList" :key="i" >
+            <q-item clickable v-ripple :to="menuItem.link">
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+          <q-separator v-if="menuItem.separator" />
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+const menuList = [
+  {
+    icon: 'stars',
+    label: 'Início',
+    separator: true,
+    link: '/test',
+  },
+  {
+    icon: 'credit_card',
+    label: 'RFID cards',
+    separator: false,
+    link: 'about',
+  },
+  {
+    icon: 'face',
+    label: 'Usuários',
+    separator: false,
+    link: 'users',
+  },
+];
+
+export default {
+  name: 'DrawerMenu',
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+      menuList,
+    };
+  },
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-page>
+    <q-page class="flex" padding>
+      <div class="q-pa-md">
+        <div class="row">
+          <h4>Dashboard</h4>
+        </div>
+        <div class="row q-gutter-x-md">
+          <div class="col">
+            <q-card bordered>
+              <q-card-section>
+                <div class="text-h6">Alo</div>
+                <div class="text-subtitle2">by Joao Braganca</div>
+              </q-card-section>
+              <q-separator />
+              <q-card-section>
+                Aloooooooooooooooooooooooooooo2
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col">
+            <q-card bordered>
+              <q-card-section>
+                <div class="text-h6">Alo</div>
+                <div class="text-subtitle2">by Joao Braganca</div>
+              </q-card-section>
+              <q-separator />
+              <q-card-section>
+                Aloooooooooooooooooooooooooooo
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col">
+            <q-card bordered>
+              <q-card-section>
+                <div class="text-h6">Alo</div>
+                <div class="text-subtitle2">by Joao Braganca</div>
+              </q-card-section>
+              <q-separator />
+              <q-card-section>
+                Aloooooooooooooooooooooooooooo
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col">
+            <q-card bordered>
+              <q-card-section>
+                <div class="text-h6">Alo</div>
+                <div class="text-subtitle2">by Joao Braganca</div>
+              </q-card-section>
+              <q-separator />
+              <q-card-section>
+                Aloooooooooooooooooooooooooooo
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </q-page>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+};
+</script>
